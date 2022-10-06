a935705bb70b5678434d713e4b8bc26810de348b
Levi <rokatee@gmail.com>
2022-10-06 17:28:24 +0900

    20221006

---
+++ WebApp03/WebContent/js/myMenu.js
@@ -0,0 +1,29 @@
+/**
+ * myMenu.js
+ */
+
+function myMenu(status)
+{
+	var menu = document.getElementById("menuTable");
+	
+	if (status==1)
+	{
+		menu.style.display = "block";
+	}
+	else
+	{
+		menu.style.display = "none";
+	}
+}
+
+/*
+function 메뉴보이기()
+{
+	
+}
+
+function 메뉴감추기()
+{
+	
+}
+*/
